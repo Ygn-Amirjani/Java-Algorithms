42e9326cc68146f3216a86ea129040e3d12e7f8c
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-02 15:38:00 +0330

    test method

--- LinkedList/test.java
+++ LinkedList/test.java
@@ -8,10 +8,25 @@ class Solution {
         list.add(8);
         list.add(5);
         list.add(3);
+        list.add(6);
+
+        list.remove();
+        list.remove(2);
 
         for(int i=0; i<list.size(); i++){
             System.out.println(list.get(i));
         }
+        System.out.println("___________________________");
+
+        list.clear();
+        
+        list.add(9);
+        list.add(7);
+
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+        System.out.println("___________________________");
         
     }
     
