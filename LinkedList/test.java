edd09ae6bb96f34c2d24810e9b08d47ac28b8a5e
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-04 17:06:27 +0330

    update test.java

--- LinkedList/test.java
+++ LinkedList/test.java
@@ -1,11 +1,7 @@
-// import java.util.LinkedList;
-
 class Solution 
 {
 
     private static final SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-    // private static LinkedList<Integer> list = new LinkedList<>();
-    
 
     public static void main(String[] args) {
 
@@ -16,28 +12,23 @@ class Solution
         list.add(6);
         list.add(13);
 
+        list.reverse();
+
+        System.out.println(list.sublist(1,4));
+        System.out.println("---"); 
+        System.out.println(list.contains(8));
+        System.out.println("---"); 
         System.out.println(list.count());
-        System.out.println("___________________________"); 
-        
+        System.out.println("---"); 
+
         list.remove();
         list.remove(2);
 
         for(int i=0; i<list.size(); i++){
             System.out.println(list.get(i));
         }
-        System.out.println("___________________________");
-
-        System.out.println(list.sublist(1,4));
-        System.out.println("___________________________");
 
         list.clear();
-        
-        list.add(9);
-        list.add(7);
-
-        System.out.println(list.contains(8));
-        System.out.println("___________________________");
-        
     }
     
     
