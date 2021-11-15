79d6082ca9e10188f45d3183e1514dffbdd97ccd
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-15 12:49:55 +0330

    delete file

---
+++ LinkedList/LinkedListCycle1.java
@@ -0,0 +1,35 @@
+/** Question link ->  https://leetcode.com/problems/linked-list-cycle/ */
+
+class ListNode {
+    // Definition for singly-linked list.
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+}
+
+public class LinkedListCycle1 {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode counter = head ;
+        while( counter != null ) {
+            /** Constraints :
+             *  -105 <= Node.val <= 105
+            */
+            if(counter.val > 100000)
+                return true ;
+
+            counter.val = 100001 ;
+            counter = counter.next ;
+        }
+
+        return false ;
+
+    }
+    
+}
+
