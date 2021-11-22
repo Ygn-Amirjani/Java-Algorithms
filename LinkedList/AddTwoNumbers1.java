32e071ac8d10b7bcd30b33e943bc0bb1c93c74c7
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-22 11:49:37 +0330

    solve addtwonumber in leet code

---
+++ LinkedList/AddTwoNumbers1.java
@@ -0,0 +1,27 @@
+/** Question link ->  https://leetcode.com/problems/linked-list-cycle/ */
+
+public class ListNode {
+    // Definition for singly-linked list.
+    int val;
+    ListNode next;
+
+    ListNode() {
+
+    }
+
+    ListNode(int val) { 
+        this.val = val; 
+    }
+
+    ListNode(int val, ListNode next) { 
+        this.val = val; this.next = next; 
+    }
+}
+
+
+public class AddTwoNumbers1 {
+    
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+    }
+}
