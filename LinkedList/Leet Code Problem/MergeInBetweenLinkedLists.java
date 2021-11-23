8a5eafdd0e02ccd9edc8d4c04981e11b6da68178
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-23 18:15:51 +0330

    not submit yet

---
+++ LinkedList/Leet Code Problem/MergeInBetweenLinkedLists.java
@@ -0,0 +1,9 @@
+/** Question link -> https://leetcode.com/problems/merge-in-between-linked-lists/ */
+
+public class MergeInBetweenLinkedLists {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+
+        return list1 ;
+            
+    }
+}
