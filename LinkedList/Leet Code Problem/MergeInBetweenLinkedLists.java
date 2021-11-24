33058fc5f1480fe97e06fd952b59443a21b278cd
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-24 16:42:16 +0330

    solution of problem in leet code

--- LinkedList/Leet Code Problem/MergeInBetweenLinkedLists.java
+++ LinkedList/Leet Code Problem/MergeInBetweenLinkedLists.java
@@ -3,7 +3,38 @@
 public class MergeInBetweenLinkedLists {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
 
-        return list1 ;
-            
+        ListNode result = new ListNode(0);
+        ListNode tmp = result ;
+        long counter = 0 ;
+
+        while (list1 != null) {
+
+            if(counter == a) {
+
+                while (list2 != null) {
+
+                    tmp.next = new ListNode(list2.val);
+                    list2 = list2.next ;
+
+                    while (counter != b) {
+                        list1 = list1.next ;
+                        counter++; 
+                    }
+                    
+                    tmp = tmp.next ;
+                }
+            }
+            else {
+
+                tmp.next = new ListNode(list1.val);
+                tmp = tmp.next ;
+            }
+
+            list1 = list1.next ;
+            counter++;  
+        }
+
+        return result.next ;     
     }
+
 }
