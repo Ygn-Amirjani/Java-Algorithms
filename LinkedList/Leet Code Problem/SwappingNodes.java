47f1690a169331fd6bca8d137999f8166af0b15c
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-24 16:42:26 +0330

    solution of problem in leet code

---
+++ LinkedList/Leet Code Problem/SwappingNodes.java
@@ -0,0 +1,31 @@
+/** Question link -> https://leetcode.com/problems/swapping-nodes-in-a-linked-list/ */
+
+public class SwappingNodes {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode fast = head ;
+        ListNode slow = head ;
+        ListNode start = head ;
+        int tmp ;
+
+        while (k > 1) {
+            fast = fast.next ;
+            k -= 1 ;
+        }
+
+        start = fast ;
+
+        while (fast.next != null) {
+            fast = fast.next ;
+            slow = slow.next ;
+        }
+
+        tmp = start.val ;
+        start.val = slow.val ;
+        slow.val = tmp ;
+
+        return head ;
+        
+    }
+
+
+}
