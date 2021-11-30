e0df610f181d0ed96ee204b93add79230185c381
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-11-30 12:49:13 +0330

    update addtwonubers2.java

--- LinkedList/Leet Code Problem/AddTwoNumbers2.java
+++ LinkedList/Leet Code Problem/AddTwoNumbers2.java
@@ -2,28 +2,27 @@
 
 public class AddTwoNumbers2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode one = size(l1) >= size(l2) ? l1 : l2 ;
-        ListNode two = size(l1) >= size(l2) ? l2 : l1 ;
         ListNode result = new ListNode(0) ;
-        ListNode tmp = result ;
-        one = reverse(one);
-        two = reverse(two);
+        ListNode tmp = result ;        
+        ListNode one =  l1 ;
+        ListNode two = l2 ;
         int divide = 0 ;
 
+        one = reverse(one);
+        two = reverse(two);
+        
         while (one != null) {
-            int x = two != null ? two.val : 0 ;
-            int sum = x + one.val + divide ;
+            int x = one != null ? one.val : 0 ;
+            int y = two != null ? two.val : 0 ;
+            int sum = x + y + divide ;
             tmp.next = new ListNode(sum % 10);
             divide = sum / 10 ;
 
-            if(two != null)
-                two = two.next ;
-
-            one = one.next ;
+            if(two != null) two = two.next ;
+            if(one != null) one = one.next ;
             tmp = tmp.next ;
         }
         
-
         if(divide > 0)
             tmp.next = new ListNode(divide) ;
 
@@ -50,15 +49,4 @@ public class AddTwoNumbers2 {
 
     }
 
-    public int size(ListNode head) {
-        ListNode tmp = head ;
-        int counter = 0 ;
-
-        while (tmp != null) {
-            tmp = tmp.next ;
-            counter += 1 ;
-        }
-
-        return counter ;
-    }
 }
