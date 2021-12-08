d732728d52b3c2250330c6a05a26fdff0b8e6b1c
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-12-08 16:41:14 +0330

    change solution

--- LinkedList/Leet Code Problem/AddTwoNumbers2.java
+++ LinkedList/Leet Code Problem/AddTwoNumbers2.java
@@ -1,51 +1,50 @@
 /** Question link -> https://leetcode.com/problems/add-two-numbers-ii/ */
 
 public class AddTwoNumbers2 {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0) ;
-        ListNode tmp = result ;        
-        ListNode one =  l1 ;
-        ListNode two = l2 ;
-        int divide = 0 ;
+    public ListNode addl2Numbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode tmp = result;
+        int divide = 0;
 
-        one = reverse(one);
-        two = reverse(two);
-        
-        while (one != null) {
-            int x = one != null ? one.val : 0 ;
-            int y = two != null ? two.val : 0 ;
-            int sum = x + y + divide ;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        while (l1 != null || l2!= null) {
+            int x = l1 != null ? l1.val : 0;
+            int y = l2 != null ? l2.val : 0;
+            int sum = x + y + divide;
             tmp.next = new ListNode(sum % 10);
-            divide = sum / 10 ;
+            divide = sum / 10;
 
-            if(two != null) two = two.next ;
-            if(one != null) one = one.next ;
-            tmp = tmp.next ;
+            if (l2 != null)
+                l2 = l2.next;
+            if (l1 != null)
+                l1 = l1.next;
+            tmp = tmp.next;
         }
-        
-        if(divide > 0)
-            tmp.next = new ListNode(divide) ;
 
+        if (divide > 0)
+            tmp.next = new ListNode(divide);
 
         return reverse(result.next);
 
     }
 
     public ListNode reverse(ListNode head) {
-        ListNode next_value = null ;
-        ListNode last_value = null ;
-        ListNode counter = head ;
+        ListNode next_value = null;
+        ListNode last_value = null;
+        ListNode counter = head;
 
-        while(counter != null) {
-            next_value = counter.next ;
-            counter.next = last_value ;
-            last_value = counter ;
-            counter = next_value ;
+        while (counter != null) {
+            next_value = counter.next;
+            counter.next = last_value;
+            last_value = counter;
+            counter = next_value;
         }
 
-        head = last_value ;
+        head = last_value;
 
-        return head ;
+        return head;
 
     }
 
