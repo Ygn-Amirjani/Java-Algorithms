40c4d6bc50c820280f85396ac3441312922d1234
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-12-09 20:05:02 +0330

    solve the leet code problem

--- LinkedList/Leet Code Problem/LinkedListComponents.java
+++ LinkedList/Leet Code Problem/LinkedListComponents.java
@@ -1,36 +1,28 @@
 /** Question link ->  https://leetcode.com/problems/linked-list-components/ */
 
 public class LinkedListComponents {
+
     public int numComponents(ListNode head, int[] nums) {
-        // The solution is wrong :(
-
         int result = 0 ;
-
-        while (head.next != null) {
+        while (head != null) {
             if (contains(nums, head.val)) {
-                if (contains(nums, head.next.val)) 
-                    result += 1 ;
-
-                else 
-                    result += 1 ;
+                result += 1 ;
+                while (head.next != null && contains(nums, head.next.val))
+                    if(head != null) head = head.next ;
                 
-
-                if (head != null) head = head.next ;
-
             }
 
-            if (head != null) head = head.next ;
+            if(head != null) head = head.next ;
         }
-
         return result ;
     }
 
-    public boolean  contains(int[] nums, int n) {
+    public boolean contains(int[] nums, int n) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == n) 
                 return true ;            
         }
-
         return false ;
     }
+    
 }
