abf06f9ba7298dd03072fdf666955802a82cd81e
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-12-09 20:05:24 +0330

    solve the leetcode problem

---
+++ LinkedList/Leet Code Problem/SwapInPairs.java
@@ -0,0 +1,23 @@
+/** Question link ->  https://leetcode.com/problems/swap-nodes-in-pairs/ */
+
+public class SwapInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null) return null ;
+
+        ListNode fast = head.next ;
+        ListNode slow = head ;
+        int tmp ;
+        while (fast != null) {
+            tmp = fast.val ;
+            fast.val = slow.val ;
+            slow.val = tmp ;
+            
+            slow = slow.next.next ;
+            fast = fast.next ;
+            if(fast != null) fast = fast.next ;
+        }
+
+        return head ;
+    }
+    
+}
