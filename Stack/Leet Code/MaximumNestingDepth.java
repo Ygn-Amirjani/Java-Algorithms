5eeb83ef7c735f0701e30553600b6c622ac80605
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2021-12-29 16:53:02 +0330

    solved

---
+++ Stack/Leet Code/MaximumNestingDepth.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class MaximumNestingDepth {
+    
+    private static final Scanner input = new Scanner(System.in) ;
+    private static final Stack<Character> stack = new Stack<>();
+    private static String str ;
+
+    public static void main(String[] args) {
+        str = input.next();
+        System.out.println(maxDepth(str));
+    }
+
+    public static int maxDepth(String s) {
+        int counter = 0,max = 0 ;
+
+        for(int i=0; i<s.length(); i+=1){
+            if(s.charAt(i) == '('){
+                stack.push('(') ;
+                counter += 1 ;
+            }
+            else{
+                if(s.charAt(i) == ')'){
+                    stack.pop();
+                    counter -= 1; 
+                }
+            }
+
+            if(counter > max) max = counter ;
+        }
+        
+        return max ;
+    }
+}
